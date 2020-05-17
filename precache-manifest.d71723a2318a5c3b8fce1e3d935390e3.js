ce5427d2266e2bfd2f759ba4963e7ef0c17938d6
oguzhan.karagol <oguzhan.karagol@trendyol.com>
2020-05-17 13:01:37 +0300

    Updates

---
+++ precache-manifest.d71723a2318a5c3b8fce1e3d935390e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1096ed72c7d1dbaf8377d4f89a6e07",
+    "url": "/live-chat/index.html"
+  },
+  {
+    "revision": "a13a010359c9aad03517",
+    "url": "/live-chat/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "b7e0fd2cc20c337780a9",
+    "url": "/live-chat/static/css/main.7db47676.chunk.css"
+  },
+  {
+    "revision": "a13a010359c9aad03517",
+    "url": "/live-chat/static/js/2.b834ad4b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/live-chat/static/js/2.b834ad4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e0fd2cc20c337780a9",
+    "url": "/live-chat/static/js/main.86356bc3.chunk.js"
+  },
+  {
+    "revision": "7e5db1e8e77ec84de3c9",
+    "url": "/live-chat/static/js/runtime-main.d63b872c.js"
+  }
+]);
